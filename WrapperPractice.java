11efe88cf00a5efa5394ff827339b1fe1eb3768c
Jonas <jonas.kaun@crpusd.org>
2026-02-05 09:11:13 -0800

    did assignment

---
+++ WrapperPractice.java
@@ -0,0 +1,23 @@
+public class WrapperPractice {
+    public static void main(String[] args){
+        Integer cook = new Integer(6);  // integer
+        Double cooked = new Double(6.7); //double
+        int valued;
+        double valuedable;
+        valued = cook.intValue();
+        valuedable = cooked.doubleValue();
+
+        
+        System.out.println(Integer.MAX_VALUE);   // max int value
+        System.out.println(Integer.MIN_VALUE);   // min int value
+
+        
+        Integer box = valued;    //boxing            
+        int boxxing = box;    // unboxing       
+
+
+
+
+    }
+    
+}
