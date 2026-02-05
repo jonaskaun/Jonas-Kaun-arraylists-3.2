a41007dd6dcc9772189ab78d7a4c36d0eba7328a
jonaskaun <jonas.kaun@crpusd.org>
2026-02-05 08:46:48 -0800

    Add files via upload

---
+++ IntegerConstructorExample.java
@@ -0,0 +1,11 @@
+/*
+* Activity 3.2.1
+*/
+public class IntegerConstructorExample
+{
+  public static void main(String[] args)
+  {
+    Integer numberOne = new Integer(1);
+    System.out.println("The value of this Integer is: " + numberOne);
+  }
+}
