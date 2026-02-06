a865022fe7caea7db511a2dceb3ecd911c29776d
Jonas <jonas.kaun@crpusd.org>
2026-02-06 09:20:16 -0800

    finished project for array lists

--- ArrayListPerson.java
+++ ArrayListPerson.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
 public class ArrayListPerson
 {  
   public static void main(String[] args)
   { 
+    ArrayList<Person> members = new ArrayList<Person>();
+    Person a = new Person();
+    Person b = new Person();
+    Person c = new Person();
+    members.add(a);
+    members.add(b);
+    members.add(c);
+    a.setname("Greg");
+    a.setsmart(5.0);
+    a.setspeed(2);
+    b.setname("Sigma");
+    b.setsmart(10.0);
+    b.setspeed(10);
+    c.setname("Goober");
+    c.setsmart(1.0);
+    c.setspeed(10);
+
+    
+    for(Person m: members){
+      System.out.print(m.getname()+", ");
+      System.out.print(m.getspeed()+", ");
+      System.out.print(m.getsmartness()+", ");
+      System.out.println("\n\n");
+    }
+    
 
   }
 }
---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private int speed;
+    private String name;
+    private double smartness;
+
+    public void setspeed(int speed){
+        this.speed = speed;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setsmart(double smartness){
+        this.smartness = smartness;
+    }
+    public String getname(){
+        return name;
+    }
+    public int getspeed(){
+        return speed;
+    }
+    public double getsmartness(){
+        return smartness;
+    }
+    
+
+
+}
